1081080f50001707f550335828e5b1f60de68f55
Ocee1 <65652920+Ocee1@users.noreply.github.com>
2021-04-18 17:25:23 +0100

    Update budget_app.py

--- budget_app.py
+++ budget_app.py
@@ -1,40 +1,58 @@
-
-
 class budget:
 
     def __init__(self, name):
         self.name = name
-        self.records = list()
+        self.records = {}
 
     def deposit(self, amount):
-        self.records.append({self.name: amount})
+        self.records[self.name] = amount
         print(self.records)
     
     
     def withdraw(self, amount):
 
-        if (self.check_funds(amount)):
-            self.records.append({"amount": -amount})
-            return True;
-        return False
+        if (self.check_funds() & self.records[self.name] != 0):
+          self.records[self.name] -= amount
+          self.get_balance()
+         
+
+
+
+
+          
 
 
     def  get_balance(self):
-        total_cash = 0
-        for item in self.records:
-            total_cash += item["amount"]
-            return total_cash
+      print(self.records[self.name])
+      
     
     def transfer(self, amount, category):
 
         if(self.check_funds(amount)):
-            self.withdraw(amount,"Transfer to " + budget.name)
-            category.deposit(amount, "transfer from " + self.name)
-            return True
-        return False
+            self.withdraw(amount)
+            category.deposit(amount)
 
 
-    #def check_funds(self, amount):
+    def check_funds(self, amount):
+      if self.records[self.name] <= amount:
+        return True
+    
 
-food = budget('rice')
-food.deposit(18)
+Food = budget(Food)
+Food.deposit(50)
+Food.withdraw(10)
+Food.get_balance()
+Food.transfer(20, Clothing)
+
+Clothing = budget(Clothing)
+Clothing.deposit(50)
+Clothing.withdraw(10)
+Clothing.get_balance()
+Clothing.transfer(20, Food)
+
+Entertainment = budget(Entertainment)
+Entertainment.deposit(50)
+Entertainment.withdraw(10)
+Entertainment.get_balance()
+Entertainment.transfer(20, Clothing)
+
